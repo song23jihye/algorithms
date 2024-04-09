72275ef1ab495e16984abecd0326d72eb24d12e3
song23jihye <songjh3578@gmail.com>
2024-04-09 21:29:44 +0900

    Other pc, after modifying, DIRECTORY MOVING FIRST and add&commit next!

--- prac_03.py
+++ prac_03.py
@@ -1,3 +1,11 @@
 arr=[[0 for j in range(4)] for i in range(3)]
 print(arr)
-print("why")
+print("shj modified test!")
+print("weird")
+print("is there no any commit?")
+arr=[[0 for j in range(4)] for i in range(3)]
+print(arr)
+arr=[[0 for j in range(4)] for i in range(3)]
+print(arr)
+arr=[[0 for j in range(4)] for i in range(3)]
+print(arr)
